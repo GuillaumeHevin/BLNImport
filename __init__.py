dab0092a9b4509c71486d6d5ba1b3d9f83679e44
HEVIN <hevin.guillaume@gmail.com>
2017-08-03 16:46:40 +0800

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ BLNImport
+                                 A QGIS plugin
+ This plugin open .bln file into a Vector Layer
+                             -------------------
+        begin                : 2017-07-14
+        copyright            : (C) 2017 by Import .BLN files
+        email                : hevin.guillaume@gmail.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load BLNImport class from file BLNImport.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .bnl_import import BLNImport
+    return BLNImport(iface)
